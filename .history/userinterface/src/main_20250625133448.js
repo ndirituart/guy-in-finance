81e8f7296200b792ec80ab14af71226e4a520257
ndirituart <ndiritupatience002@gmail.com>
2025-06-29 19:34:34 +0300

    Trying to recover userinterface files

--- .history/userinterface/src/main_20250625133448.js
+++ .history/userinterface/src/main_20250625133448.js
@@ -1,9 +1,9 @@
 // main.js
 import { createApp } from 'vue';
 import App from './App.vue';
-//for global functionality
-
-import router from './router';
+//for global functionalityof AccountBalance on different pages
+import { createPinia } from 'pinia';
+import router from '../router';
 
 // FontAwesome setup
 import { library } from '@fortawesome/fontawesome-svg-core';
@@ -15,7 +15,10 @@ import { faUser, faHome, faArrowRight } from '@fortawesome/free-solid-svg-icons'
 library.add(faUser, faHome, faArrowRight); // add icons to the library
 
 const app = createApp(App);
+//for global functionality
+const pinia = createPinia();
 
 app.component('font-awesome-icon', FontAwesomeIcon); // register globally
 app.use(router);
+app.use(pinia); // use Pinia for global state management
 app.mount('#app');
